4c8258bc8c2df2f57b6313edcf2e5f754d74ff10
Arky13 <polygot882@gmail.com>
2025-04-29 17:31:23 +0530

    Project structure ready,setup Virtual COnda env done

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+polygot882
+ff5zWWW4rMD16DZZ
+
+conections tring -- 
+mongodb+srv://polygot882:<db_password>@cluster0.erixzdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aryan Arya",
+    author_email="polygot882@gmail.com",
+    packages=find_packages()
+)
